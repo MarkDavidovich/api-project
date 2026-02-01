8abd2912f4fd8e494e63a306e0b148e1e9322b24
Mark <mark.davidovich@gmail.com>
2026-02-01 20:06:46 +0200

    finish model file and cleanup

--- model.js
+++ model.js
@@ -1,15 +1,16 @@
 export const DataModel = () => {
-  const _randomData = [];
+  const _MAX_POKEMON_ID = 1025;
 
   const getRandomData = async () => {
     try {
       const [users, quote, pokemon, text] = await Promise.all([
         fetch(`https://randomuser.me/api/?results=7&inc=name,location,picture`).then((r) => r.json()),
         fetch(`https://api.kanye.rest`).then((r) => r.json()),
-        fetch(`https://pokeapi.co/api/v2/pokemon/${_getRandomInt(1, 1025)}/`).then((r) => r.json()),
-        fetch(`https://baconipsum.com/api/?type=meat-and-filler&paras=1`).then((r) => r.json()),
+        fetch(`https://pokeapi.co/api/v2/pokemon/${_getRandomInt(1, _MAX_POKEMON_ID)}/`).then((r) => r.json()),
+        fetch(`https://baconipsum.com/api/?type=meat-and-filler&sentences=3`).then((r) => r.json()),
       ]);
-      //   console.log(quote.quote); formatted Quote
+
+      return { users: _formatUserData(users), quote: quote.quote, pokemon: _formatPokemonData(pokemon), text: text[0] };
     } catch (err) {
       console.error(`Error: ${err.message}`);
     }
@@ -27,8 +28,8 @@ export const DataModel = () => {
 
   const _formatPokemonData = (pokemon) => {
     return {
-        name:_capitalize(pokemon.name),
-        pictureUrl: pokemon.sprites.front_default;
+      name: _capitalize(pokemon.name),
+      pictureUrl: pokemon.sprites.front_default,
     };
   };
 
@@ -42,6 +43,3 @@ export const DataModel = () => {
 
   return { getRandomData };
 };
-
-const dataModel = DataModel();
-// dataModel.getRandomData();
