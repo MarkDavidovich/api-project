eabb1c8c4034ca18dbe795669898c611bbd87311
Mark <mark.davidovich@gmail.com>
2026-02-01 19:43:45 +0200

    add model with api calls and basic user data formatting function

---
+++ model.js
@@ -0,0 +1,36 @@
+export const DataModel = () => {
+  const _randomData = [];
+
+  const getRandomData = async () => {
+    try {
+      const [users, quote, pokemon, text] = await Promise.all([
+        fetch(`https://randomuser.me/api/?results=7&inc=name,location,picture`).then((r) => r.json()),
+        fetch(`https://api.kanye.rest`).then((r) => r.json()),
+        fetch(`https://pokeapi.co/api/v2/pokemon/${_getRandomInt(1, 1025)}/`).then((r) => r.json()),
+        fetch(`https://baconipsum.com/api/?type=meat-and-filler&paras=1`).then((r) => r.json()),
+      ]);
+      //   console.log(quote.quote); formatted Quote
+    } catch (err) {
+      console.error(`Error: ${err.message}`);
+    }
+  };
+
+  const _formatUserData = (users) => {
+    return users.results.map((user) => ({
+      firstName: user.name.first,
+      lastName: user.name.last,
+      city: user.location.city,
+      state: user.location.state,
+      pictureUrl: user.picture.large,
+    }));
+  };
+
+  const _getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  return { getRandomData };
+};
+
+const dataModel = DataModel();
+dataModel.getRandomData();
