5b592af93dce359f29013b84579321434c178bc9
Mark <mark.davidovich@gmail.com>
2026-02-01 22:22:52 +0200

    add helper function to create friend elements dynamically

--- view.js
+++ view.js
@@ -4,6 +4,8 @@ export const Renderer = () => {
   };
 
   const _renderUserInfo = (users) => {
+    const friendList = users.slice(1);
+
     const userImg = document.querySelector(".user-img");
     const userName = document.querySelector(".user-name");
     const userLocation = document.querySelector(".user-location");
@@ -11,7 +13,32 @@ export const Renderer = () => {
     userImg.setAttribute("src", users[0].pictureUrl);
     userName.textContent = `${users[0].firstName} ${users[0].lastName}`;
     userLocation.textContent = `${users[0].city}, ${users[0].state}`;
-    //include friends?
+
+    for (let friend of friendList) {
+    }
+  };
+
+  const _createFriend = (firstName, lastName, pictureUrl) => {
+    const friendElement = document.createElement("li");
+    const imgContainer = document.createElement("div");
+    const friendImg = document.createElement("img");
+    const friendName = document.createElement("p");
+
+    friendElement.classList.add("friend");
+    imgContainer.classList.add("friend-img-container");
+    friendImg.classList.add("friend-img");
+    friendName.classList.add("friend-name");
+
+    friendImg.setAttribute("src", pictureUrl);
+    friendImg.setAttribute("alt", `${firstName}'s picture`);
+
+    friendName.textContent = `${firstName} ${lastName}`;
+
+    imgContainer.appendChild(friendImg);
+    friendElement.appendChild(imgContainer);
+    friendElement.appendChild(friendName);
+
+    return friendElement;
   };
 
   return { renderUserPage };
