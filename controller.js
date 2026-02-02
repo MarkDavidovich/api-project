9731c8109e110df6b3b5ebb6a6c609ed08d90ff4
Mark <mark.davidovich@gmail.com>
2026-02-02 16:16:01 +0200

    add function to load saved profiles from menu

--- controller.js
+++ controller.js
@@ -7,10 +7,14 @@ const dataModel = DataModel();
 const generateButton = document.querySelector(".generate-btn");
 const saveButton = document.querySelector(".save-btn");
 const loadButton = document.querySelector(".load-btn");
+const loadMenu = document.querySelector(".load-menu");
 
 const loadAndRenderData = async () => {
   const randomUserData = await dataModel.getRandomUserData();
   renderer.renderUserPage(randomUserData);
+};
+
+const loadAndRenderSavedUsers = () => {
   renderer.renderSavedUsers(dataModel.getSavedProfiles());
 };
 
@@ -18,18 +22,21 @@ generateButton.addEventListener("click", loadAndRenderData);
 
 saveButton.addEventListener("click", () => {
   dataModel.saveCurrentProfile();
+  loadAndRenderSavedUsers();
 });
-// saveButton.addEventListener("click", () => {
-//   dataModel.saveData();
-// });
-
-// loadButton.addEventListener("click", () => {
-//   const data = dataModel.loadData();
-//   renderer.renderUserPage(data);
-// });
 
 loadButton.addEventListener("click", () => {
   renderer.toggleLoadMenu();
+  loadAndRenderSavedUsers();
 });
 
-// loadAndRenderData();
+loadMenu.addEventListener("click", (ev) => {
+  if (ev.target.classList.contains("saved-user")) {
+    const id = +ev.target.dataset.id;
+
+    const userPage = dataModel.loadSavedProfile(id);
+    renderer.renderUserPage(userPage);
+  }
+});
+
+loadAndRenderData();
